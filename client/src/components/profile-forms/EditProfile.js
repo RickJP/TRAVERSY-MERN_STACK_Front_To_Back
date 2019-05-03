c415b9f8ca18135bd4f8623f353d6345072e6d4a
Rick <3741326+RickJP@users.noreply.github.com>
2019-05-04 00:32:08 +0900

    50. Add Education & Experiences

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -64,6 +64,7 @@ const EditProfile = ({ profile: { profile, loading }, createProfile, getCurrentP
 	const onSubmit = (e) => {
 		e.preventDefault();
 		createProfile(formData, history, true);
+		history.push('/dashboard');
 	};
 
 	return (
