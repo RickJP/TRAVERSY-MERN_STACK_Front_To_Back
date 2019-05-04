4e13a33864d3112c5f1f46d4e30161dc29730e43
Rick <3741326+RickJP@users.noreply.github.com>
2019-05-04 13:43:15 +0900

    53. Finish Profile Actions & Reducer

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, CLEAR_PROFILE, ACCOUNT_DELETED } from './types';
+import {
+	GET_PROFILE,
+	PROFILE_ERROR,
+	GET_PROFILES,
+	UPDATE_PROFILE,
+	CLEAR_PROFILE,
+	ACCOUNT_DELETED,
+	GET_REPOS
+} from './types';
 
 // Get current users profile
 export const getCurrentProfile = () => async (dispatch) => {
@@ -10,6 +18,59 @@ export const getCurrentProfile = () => async (dispatch) => {
 
 		dispatch({
 			type: GET_PROFILE,
+			payload: res.data
+		});
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
+
+// Get All Profiles
+export const getProfiles = () => async (dispatch) => {
+	dispatch({ type: CLEAR_PROFILE });
+
+	try {
+		const res = await axios.get('/api/profile');
+
+		dispatch({
+			type: GET_PROFILES,
+			payload: res.data
+		});
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
+
+// Get Profile by ID
+export const getProfileById = (userId) => async (dispatch) => {
+	try {
+		const res = await axios.get(`/api/profile/${userId}`);
+
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data
+		});
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
+
+// Get Github Repos
+export const getGithubRepos = (username) => async (dispatch) => {
+	try {
+		const res = await axios.get(`/api/profile/github/${username}`);
+
+		dispatch({
+			type: GET_REPOS,
 			payload: res.data
 		});
 	} catch (err) {
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -140,7 +140,6 @@ router.delete('/', auth, async (req, res) => {
 	try {
 		// Remove User Posts
 		await Post.deleteMany({ user: req.user.id });
-
 		// Remove profile
 		await Profile.findOneAndRemove({ user: req.user.id });
 		// Remove user
