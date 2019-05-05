b43a8ee1bb0bf9add202b589d6db5de8f21a14c1
Rick <3741326+RickJP@users.noreply.github.com>
2019-05-05 10:48:42 +0900

    61. Post Item Component

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import {setAlert} from './alert';
-import {GET_POSTS, POST_ERROR} from '.types';
+import {GET_POSTS, POST_ERROR} from './types';
 
 // Ger Posts
 export const getPosts = () => async dispatch => {
   try {
-    const res = axios.get('/api/posts');
+    const res = await axios.get('/api/posts');
 
     dispatch({
       type: GET_POSTS,
